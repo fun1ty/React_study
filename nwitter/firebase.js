c282904bb9328202250a1178846d48d1fe42c130
kmr <ettedlop@gmail.com>
2023-03-20 20:20:13 +0900

    first commit

---
+++ nwitter/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAtDWMRR8JI5ZJkyEd5si8mPFDoFiHMNU",
+    authDomain: "nwitter-6aa26.firebaseapp.com",
+    projectId: "nwitter-6aa26",
+    storageBucket: "nwitter-6aa26.appspot.com",
+    messagingSenderId: "285963886088",
+    appId: "1:285963886088:web:bb6bd97cbfd75f9b0d3bbd"
+  };
+  export const app = initializeApp(firebaseConfig);
